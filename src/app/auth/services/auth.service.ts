debe0e4dfaf958beb53cd1d2de7ad6660d9426ad
2014maximo <alexuriel.777@gmail.com>
2023-09-30 00:09:41 -0500

    servicio guard

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = environment.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User|undefined {
+    if(!this.user ) return undefined;
+    return structuredClone( this.user );
+  }
+
+  login( email: string, password: string ):Observable<User>{
+    
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', user.id.toString() ))
+      )
+  }
+
 }
